ade6f95c5daaa5eef9b1018f25c2791b58f1ea57
bamboi-andrei <95073530+bamboi-andrei@users.noreply.github.com>
2022-10-04 15:54:18 +0300

    Initialize the api

---
+++ api.py
@@ -0,0 +1,12 @@
+import flask
+import requests
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def get_doc():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+app.run()
