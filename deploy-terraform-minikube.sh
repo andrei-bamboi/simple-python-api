0c51fa91bec7094a55ded200dcde2cb8e77508e8
bamboi-andrei <95073530+bamboi-andrei@users.noreply.github.com>
2022-10-09 03:27:33 +0300

    add deploy script

---
+++ deploy-terraform-minikube.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set +x
+
+cd iac
+
+if [[ -f .terraform.lock.hcl ]]; then
+  rm -f .terraform.lock.hcl
+fi
+
+if [[ -d .terraform ]]; then
+  rm -rf .terraform
+fi
+
+declare "action"="$1"
+
+case "${action}" in
+  "deploy")
+    terraform init
+    terraform apply -auto-approve
+    if [ $? -ne 0 ]; then
+      echo "failed"
+      exit 1
+    fi
+    ;;
+  "destroy")
+    terraform init
+
+    terraform destroy -auto-approve
+    if [ $? -ne 0 ]; then
+      echo "failed"
+      exit 1
+    fi
+    ;;
+  "plan")
+    terraform init
+    terraform plan
+    if [ $? -ne 0 ]; then
+      echo "failed"
+      exit 1
+    fi
+    ;;
+  *)
+  echo -e "${action} action is an invalid option\nShould choose between "deploy", "plan" or "destroy" actions"
+  ;;
+esac
+
+exit 0
+
+
